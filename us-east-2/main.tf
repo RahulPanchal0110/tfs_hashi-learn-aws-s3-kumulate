28e3b4d6bfafee7252f3ffee3a6ec8fe0abc64ba
Vitali Bystritski <vbystritski@sbgtv.com>
2023-02-13 16:57:05 -0500

    moving main files into subfolder for TFC trigger

---
+++ us-east-2/main.tf
@@ -0,0 +1,39 @@
+################################################################################
+##### Terraform Providers and Backend ##########################################
+################################################################################
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws" #### aws provider
+      version = "4.54.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.3.0"
+    }
+  }
+  cloud {
+    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
+    organization = "sbgtv"
+    workspaces {
+      tags = ["kumulate"]
+    }
+  }
+}
+################################################################################
+####### Import data from remote state kms ######################################
+################################################################################
+# data "terraform_remote_state" "kms" {
+#   backend = "remote"
+#   config = {
+#     hostname     = "app.terraform.io"
+#     organization = "sbgtv"
+#     workspaces = {
+#       name = ""
+#     }
+#   }
+# }
+################################################################################
+################################################################################
+################################################################################
